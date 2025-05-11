b458870abd2f2a59446dcb5c4ed4a831ff5d4890
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-05-10 20:50:58 -0400

    Add files via upload

--- Fitness_homie/Intro.py
+++ Fitness_homie/Intro.py
@@ -33,9 +33,9 @@ def checkogin():
             if username == checkuser and password == checkpassword:
                 return render_template("main.html")
             else:
-                return render_template("login.html", check = "Password wrong")
+                return render_template("login.html", check = "Password or username wrong")
         else:
-            return render_template("login.html", check = "Account doesnt exist")
+            return render_template("login.html", check = "Account doesnt exist, please create an account")
     
 @app.route("/signup", methods = ["GET","POST"])
 def signup():
@@ -46,31 +46,44 @@ def signup():
         return render_template("signup.html")
 @app.route("/information", methods = ["GET","POST"])   
 def check():
-    print("please work")
     global username, password
     fileDir = os.path.dirname(os.path.realpath("__file__"))
     username = request.form.get("username")
     password = request.form.get("password")
-    print("I do in fact work ")
-    if username == "" or password == "":
+    email = request.form.get("email")
+    if username == "" or password == "" or email == "":
         return render_template("signup.html")
     else:
         checklen = len(password)
         if checklen < 6:
             return render_template("signup.html")
         else:
-            filename = username + ".doc"
-            fileexist = bool(path.exists(filename))
-            if fileexist == True:
-                print("works")
-                return render_template("login.html", check = "Login already exists, please login")
-            else:
-                admin = open(filename, "x")
-                print("file created")
-                admin.write(username + "\n" + password)
-                admin.close()
-
-    return render_template("information.html")
+            check = email.isdigit()
+            if check == True:
+                return render_template("signup.html", check = "Email cannot be just numbers")
+            else:                
+                filename = username + ".doc"
+                filename2 = username +"weightinfo" +".doc"
+                filename3 = username + "cart" + ".doc"
+                filename4 = username + "workout" + ".doc"
+                filename5 = username + "food" + ".doc"
+                fileexist = bool(path.exists(filename))
+                if fileexist == True:
+                    return render_template("login.html", check = "Login already exists, please login")
+                else:
+                    admin = open(filename, "x")
+                    admin.write(username + "\n" + password + "\n" + email)
+                    admin.close()
+                    admin2 = open(filename2,  "x")
+                    admin2.close()
+                    admin3 = open(filename3, "x")
+                    admin3.close()
+                    admin4 = open(filename4, "x")
+                    admin4.close()
+                    admin5 = open(filename5, "x")
+                    admin5.close()
+                    
+        return render_template("information.html")
 
 if __name__ == "__main__":
     app.run()
