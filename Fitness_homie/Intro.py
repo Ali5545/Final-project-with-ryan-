ad9e417f80c6e9fda0a4d77bf5984b5ae96a0cb1
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-05-12 21:50:52 -0400

    Add files via upload

--- Fitness_homie/Intro.py
+++ Fitness_homie/Intro.py
@@ -84,6 +84,12 @@ def check():
                     admin5.close()
                     
         return render_template("information.html")
+@app.route("/home", methods = ["GET", "POST"])
+def home():
+    return render_template("main.html")
 
+@app.route("/profile", methods = ["GET", "POST"])
+def profile():
+    return render_template("main.html")
 if __name__ == "__main__":
     app.run()
