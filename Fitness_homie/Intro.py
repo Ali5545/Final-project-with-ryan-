b2051ddc403940bbade170838396a09d536777f8
Rz116 <147172188+Rz116@users.noreply.github.com>
2025-05-15 20:45:31 -0400

    Add files via upload

--- Fitness_homie/Intro.py
+++ Fitness_homie/Intro.py
@@ -43,7 +43,7 @@ def signup():
         return render_template("signup.html")
     else:
         check()
-        return render_template("signup.html")
+        return render_template("information.html")
 @app.route("/information", methods = ["GET","POST"])   
 def check():
     global username, password
@@ -63,7 +63,6 @@ def check():
                 return render_template("signup.html", check = "Email cannot be just numbers")
             else:                
                 filename = username + ".doc"
-                filename2 = username +"weightinfo" +".doc"
                 filename3 = username + "cart" + ".doc"
                 filename4 = username + "workout" + ".doc"
                 filename5 = username + "food" + ".doc"
@@ -74,16 +73,59 @@ def check():
                     admin = open(filename, "x")
                     admin.write(username + "\n" + password + "\n" + email)
                     admin.close()
-                    admin2 = open(filename2,  "x")
-                    admin2.close()
                     admin3 = open(filename3, "x")
                     admin3.close()
                     admin4 = open(filename4, "x")
                     admin4.close()
                     admin5 = open(filename5, "x")
                     admin5.close()
-                    
-        return render_template("information.html")
+        return information()       
+    
+@app.route("/infohome", methods = ["GET", "POST"])
+def information():
+    filename = username + ".doc"
+    if request.method == "POST":
+        return render_template("information.html")    
+    else:
+        age = request.form.get("age")
+        feet = request.form.get("Feet")
+        inches = request.form.get("Inches")
+        weight = request.form.get("weight")
+        goal = request.form.get("goal")
+        gender = request.form.get("gender")
+        if age == "" or feet == "" or inches == "" or weight == "" or goal == "" or int(feet) > 12 or int(feet) < 1:
+            return render_template("information.html", check = "Your height must be between 1 and 12 Feet")
+        else:
+            check = list(age)
+            for i in range(len(check)):
+                num = ord(check[i])
+                if num < 48 or num > 57:
+                    return render_template("information.html", check = "You must type in a number for your age!")
+            check2 = list(feet)
+            for i in range(len(check2)):
+                num2 = ord(check2[i])
+                if num2  < 48 or num2 > 57:
+                    return render_template("information.html", check = "You must type in a number for your Height!")
+            check3 = list(inches)
+            for i in range(len(check3)):
+                num3 = ord(check3[i])
+                if num3 < 48 or num3 > 57:
+                    return render_template("information.html", check = "You must type in a number for your Height!")
+            check4 = list(weight)
+            for i in range(len(check4)):
+                num4 = ord(check4)
+                if num4 < 48 or num4 > 57:
+                     return render_template("information.html", check = "You must type in a number for your weight!")
+            height = int(feet) * 12 + int(inches)
+            if gender == "Man":
+                Bmr = 66.47 + 6.24 * int(weight) + 12.7 * int(height) - 6.755 * int(age)
+            else:
+                Bmr = 655.1 + 4.35 * int(weight) + 4.7 * int(height) - 4.7 * int(age)
+
+        return render_template("main.html")
+
+
+           
 @app.route("/home", methods = ["GET", "POST"])
 def home():
     return render_template("main.html")
